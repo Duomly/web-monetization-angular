c5519ac474096fb7f900b62c6a9bf8aef87135f5
rfabisiak <fabisiakradoslaw@gmail.com>
2020-05-11 15:43:38 +0200

    Document interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+declare global {
+  interface Document {
+    monetization?: any;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent implements OnInit {
   title = 'web-monetization';
   monetized = false;
