2ab2d4868c4df703b61ff27fd4a082dcb4486d4a
rfabisiak <fabisiakradoslaw@gmail.com>
2020-05-11 15:36:14 +0200

    Web monetization example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'web-monetization';
+  monetized = false;
+
+  ngOnInit() {
+    if (document.monetization) {
+      document.monetization.addEventListener('monetizationstart', () => {
+        this.monetized = true;
+      })
+    }
+  }
+
+  pay() {
+    const  event = new Event('monetizationstart');
+    document.monetization.dispatchEvent(event)
+  }
 }
